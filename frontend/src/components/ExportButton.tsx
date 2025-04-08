f6d04860a341c3ed2f0d72f32700d98c3f9515a5
tenkus47 <tenkus47@gmail.com>
2025-04-08 15:06:42 +0530

    add docx export

--- frontend/src/components/ExportButton.tsx
+++ frontend/src/components/ExportButton.tsx
@@ -1,6 +1,6 @@
 import { useEditor } from "@/contexts/EditorContext";
-import { GrDocumentTxt } from "react-icons/gr";
-
+import { GrDocumentTxt, GrDocumentWord } from "react-icons/gr";
+import * as quillToWord from "quill-to-word";
 function ExportButton({ doc_id }: { readonly doc_id: string }) {
   const { getQuill } = useEditor();
   const quill = getQuill(doc_id);
@@ -15,10 +15,31 @@ function ExportButton({ doc_id }: { readonly doc_id: string }) {
       URL.revokeObjectURL(a.href);
     }
   };
+
+  const exportWord = async () => {
+    if (quill) {
+      const content = quill.getContents();
+      const quillToWordConfig = {
+        exportAs: "blob",
+      };
+      const blob = await quillToWord.generateWord(content, quillToWordConfig);
+      const a = document.createElement("a");
+      a.href = URL.createObjectURL(blob);
+      a.download = "exported_text.docx";
+      a.click();
+      URL.revokeObjectURL(a.href);
+    }
+  };
+
   return (
-    <button onClick={exportText}>
-      <GrDocumentTxt />
-    </button>
+    <>
+      <button onClick={exportText}>
+        <GrDocumentTxt />
+      </button>
+      <button onClick={exportWord}>
+        <GrDocumentWord />
+      </button>
+    </>
   );
 }
 
