b0f4d3264170f25ab71a10aa44addf49576e6dbc
tenkus47 <tenkus47@gmail.com>
2025-05-13 17:13:43 +0530

    add userId

--- frontend/src/components/matomoTracker.ts
+++ frontend/src/components/matomoTracker.ts
@@ -1,16 +1,19 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useAuth } from '@/auth/use-auth-hook';
 
 function MatomoTracker() {
   const location = useLocation();
-
+  const { currentUser } = useAuth();
+  const userId = currentUser?.id ?? 'user-123';
   useEffect(() => {
     if (window._paq) {
       window._paq.push(['setCustomUrl', window.location.href]);
       window._paq.push(['setDocumentTitle', document.title]);
       window._paq.push(['trackPageView']);
+      window._paq.push(['setUserId', userId]);
     }
-  }, [location]);
+  }, [location, userId]);
 
   return null;
 }
