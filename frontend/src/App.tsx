6f404f64aea02eec3b127d7142b32e0ef36c5859
tenkus47 <tenkus47@gmail.com>
2025-06-19 16:30:51 +0530

    refactor: clean up App component by removing commented-out Matomo tracking code and unnecessary imports for improved readability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Routes, Route, Navigate, Outlet } from "react-router-dom";
-import { lazy, Suspense, useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
 import { AuthProvider } from "./auth/auth-context-provider";
 import { useAuth } from "./auth/use-auth-hook";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
@@ -7,9 +7,7 @@ import Callback from "./pages/Callback";
 import Login from "./pages/Login";
 import DocumentsWrapper from "./components/DocumentWrapper";
 import Navbar from "./components/Dashboard/Navbar";
-// import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
-// import RouteTracker from "./components/RouteTracker";
-// Lazy loaded components
+
 const ProjectList = lazy(() => import("./components/Dashboard/ProjectList"));
 const QuillVersionProvider = lazy(() =>
   import("./contexts/VersionContext").then((module) => ({
@@ -18,27 +16,6 @@ const QuillVersionProvider = lazy(() =>
 );
 
 const queryClient = new QueryClient();
-
-// const instance = createInstance({
-//   urlBase: "https://track.pecha.org/",
-//   siteId: 3,
-//   // userId: 'UID76903202', // optional, default value: `undefined`.
-//   // trackerUrl: 'https://LINK.TO.DOMAIN/tracking.php', // optional, default value: `${urlBase}matomo.php`
-//   srcUrl: "https://track.pecha.org/matomo.js", // optional, default value: `${urlBase}matomo.js`
-//   disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-//   heartBeat: {
-//     active: true, // optional, default value: true
-//     seconds: 20, // optional, default value: `15
-//   },
-//   linkTracking: false, // optional, default value: true
-//   configurations: {
-//     // optional, default value: {}
-//     // any valid matomo configuration, all below are optional
-//     disableCookies: false, //required concent
-//     setSecureCookie: true,
-//     setRequestMethod: "POST",
-//   },
-// });
 
 function Layout({ children }: { children: React.ReactNode }) {
   const { isAuthenticated, login, isLoading, getToken } = useAuth();
@@ -104,9 +81,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        {/* <MatomoProvider value={instance}> */}
         <AppContent />
-        {/* </MatomoProvider> */}
       </AuthProvider>
     </QueryClientProvider>
   );
