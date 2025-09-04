bac84d71f14248af4b9cf290297fd8756db71dd1
tenkus47 <tenkus47@gmail.com>
2025-09-04 11:32:30 +0530

    style(UI): update button styles and layout in index.html, enhance SettingsButton with new icon and size adjustments

--- frontend/src/components/setting/SettingsButton.tsx
+++ frontend/src/components/setting/SettingsButton.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Settings } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { useTranslate } from "@tolgee/react";
 import SettingsModal from "./SettingsModal";
 import { useTranslationSidebarParams } from "@/hooks/useQueryParams";
 import { useParams } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { GrSettingsOption } from "react-icons/gr";
 
 const SettingsButton: React.FC = () => {
   const { t } = useTranslate();
@@ -18,10 +18,10 @@ const SettingsButton: React.FC = () => {
     >
       <Button
         variant="ghost"
-        size="sm"
-        className="cursor-pointer flex gap-1 text-gray-500 hover:text-gray-900 px-2 items-center hover:bg-transparent"
+        size="xs"
+        className=" cursor-pointer flex gap-1 text-neutral-200 hover:text-neutral-400 px-2 items-center hover:bg-transparent"
       >
-        <Settings size={16} />
+        <GrSettingsOption size={14} />
         <span className="text-sm">{t("common.settings", "Settings")}</span>
       </Button>
     </SettingsModal>
