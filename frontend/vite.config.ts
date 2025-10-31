500cfa29449e6e3e2a7914e33c6da2f53317188c
tenkus47 <tenkus47@gmail.com>
2025-10-31 18:59:42 +0530

    chore: update package.json and package-lock.json for dependency management and optimization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,50 +5,50 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
 
-	const target_url = env.VITE_SERVER_URL || "http://localhost:9000";
+  const target_url = env.VITE_SERVER_URL || "http://localhost:9000";
 
-	return {
-		plugins: [react(), tailwindcss()],
-		optimizeDeps: {
-			exclude: ["react-icons"],
-		},
-		server: {
-			port: 3000,
-			allowedHosts: true,
-			proxy: {
-				"/api": {
-					target: target_url,
-					changeOrigin: true,
-					secure: false,
-				},
-				"/projects": {
-					target: target_url,
-					changeOrigin: true,
-					secure: false,
-				},
-				"/users": {
-					target: target_url,
-					changeOrigin: true,
-					secure: false,
-				},
-				"/comments": {
-					target: target_url,
-					changeOrigin: true,
-					secure: false,
-				},
-				"/versions": {
-					target: target_url,
-					changeOrigin: true,
-					secure: false,
-				},
-			},
-		},
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src"),
-			},
-		},
-	};
+  return {
+    plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      exclude: ["react-icons"],
+    },
+    server: {
+      port: 3000,
+      allowedHosts: true,
+      proxy: {
+        "/api": {
+          target: target_url,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/projects": {
+          target: target_url,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/users": {
+          target: target_url,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/comments": {
+          target: target_url,
+          changeOrigin: true,
+          secure: false,
+        },
+        "/versions": {
+          target: target_url,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
